c2525eb358219da328b57a6792854df90ce8cc65
endot <endot04@hotmail.com>
2024-01-29 23:08:34 -0300

    mi primer commit

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -28,6 +28,11 @@ function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   if(num < 90) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function mayorQueCincuenta(num) {
