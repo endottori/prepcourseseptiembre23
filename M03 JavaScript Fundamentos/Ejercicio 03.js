7b48442bff1a86b44b067d75943564bed2ac12f0
endot <endot04@hotmail.com>
2024-01-31 00:37:29 -0300

    mi primer commit

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -50,12 +50,22 @@ function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num % 2 === 0){
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   if(num % 2 !== 0){
+      return true;
+   } else {
+      return false;
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
